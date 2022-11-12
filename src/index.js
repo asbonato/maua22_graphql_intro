83f05f69c97c4efbe45bb33512ed16099724021f
asbonato <asbonato@gmail.com>
2022-11-12 08:48:20 -0300

    apostila 2 finalizada

--- src/index.js
+++ src/index.js
@@ -1,11 +1,71 @@
 import { createServer } from "@graphql-yoga/node"
 
+const usuarios = [
+    {
+        id:'100',
+        nome:'José',
+        livros:[
+            {
+                id:'1',
+                titulo:'Effective Java',
+                genero:'Técnico',
+                edicao:3,
+                preco:39.99
+            },
+            {
+                id:'2',
+                titulo:'Concrete Mathematics',
+                genero:'Técnico',
+                edicao:1,
+                preco:89.99
+            }
+        ]
+    },
+    {
+        id:'101',
+        nome:'Maria',
+        livros:[
+            {
+                id:'5',
+                titulo:'Programming Challenges',
+                genero:'Técnico',
+                edicao:1,
+                preco:39.99
+            }
+        ]
+    }
+]
+const livros=[
+    {
+        id:'1',
+        titulo:'Effective Java',
+        genero:'Técnico',
+        edicao:3,
+        preco:39.99
+    },
+    {
+        id:'2',
+        titulo:'Concrete Mathematics',
+        genero:'Técnico',
+        edicao:1,
+        preco:89.99
+    }
+]
+
 const typeDefs = `
     type Query {
+        usuarios:[Usuario!]!
+        livros(precoMaximo: Float!): [Livro!]!
         adicionar(numeros:[Float!]):Float!
         notas: [Int!]!
         bemVindo(nome: String): String!
         effectiveJava: Livro!
+    },
+    type Usuario {
+        id: ID!,
+        nome: String!,
+        idade: Int!,
+        livros: [Livro!]
     },
     type Livro {
         id: ID!
@@ -18,6 +78,14 @@ const typeDefs = `
 
 const resolvers = {
     Query: {
+        usuarios(){
+            return usuarios;
+        },
+        livros(parent, args, ctx, info){
+            return livros.filter((l) => {
+                return l.preco <= args.precoMaximo
+            });
+        },
         adicionar(parent, args, ctx, info){
             return args.numeros.length === 0? 0 :
             args.numeros.reduce((ac,atual) => {
